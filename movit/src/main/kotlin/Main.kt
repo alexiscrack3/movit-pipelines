99c0d88d415e905a964930be63ea6f6453fac0d7
Alexis Ortega <alexis.oc3@gmail.com>
2024-06-28 13:46:59 -0400

    Get data from http request

--- movit/src/main/kotlin/Main.kt
+++ movit/src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.alexiscrack3
 
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okio.IOException
 import org.apache.beam.runners.direct.DirectRunner
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.coders.StringUtf8Coder
@@ -26,6 +30,35 @@ class LineFormatter: DoFn<String, String>() {
     }
 }
 
+class ApiFetcher: DoFn<String, String>() {
+    @ProcessElement
+    fun processElement(@Element line: String, out: OutputReceiver<String?>) {
+        val apiKey = System.getenv("API_KEY") ?: throw IllegalStateException("API_KEY environment variable not set")
+        val apiSecret = System.getenv("API_SECRET") ?: throw IllegalStateException("API_SECRET environment variable not set")
+        val client = OkHttpClient()
+        val url = HttpUrl.Builder()
+            .scheme("https")
+            .host("runsignup.com")
+            .addPathSegment("rest")
+            .addPathSegment("races")
+            .addQueryParameter("api_key", apiKey)
+            .addQueryParameter("api_secret", apiSecret)
+            .addQueryParameter("format", "json")
+            .build()
+        val request = Request.Builder()
+            .url(url)
+            .get()
+            .build()
+
+        val response = client.newCall(request).execute()
+        if (!response.isSuccessful) {
+            throw IOException("Unexpected code ${response.code}")
+        }
+
+        val data = response.body?.string()
+        out.output(data)
+    }
+}
 
 fun main() {
     val options = PipelineOptionsFactory.create()
@@ -35,7 +68,8 @@ fun main() {
     val input: PCollection<String> = p.apply(TextIO.read().from("input.txt"))
 //    val input: PCollection<String> = p.apply(Create.of(LINES)).setCoder(StringUtf8Coder.of())
 
-    val transformation: ParDo.SingleOutput<String, String> = ParDo.of(LineFormatter())
+//    val transformation: ParDo.SingleOutput<String, String> = ParDo.of(LineFormatter())
+    val transformation: ParDo.SingleOutput<String, String> = ParDo.of(ApiFetcher())
     val output: PCollection<String> = input.apply(transformation)
     output.apply("WriteToText", TextIO.write().to("build/myfile").withSuffix(".txt"));
 
